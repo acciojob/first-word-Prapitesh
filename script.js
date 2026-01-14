3ed2c689bfb7bb8541f1079567cc3fc5e8b72e66
Prapitesh <116258736+Prapitesh@users.noreply.github.com>
2026-01-14 12:43:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function firstWord(s) {
-  if(s==='') return '';
-  let i=s.indexOf(' ');
-  if(i==-1){
-    return s;
+  
 }
-  return s.substring(0,i);
-}
+
 // Do not change the code below
 
 const s = prompt("Enter String:");
