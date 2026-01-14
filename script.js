6bb0770cef594361f4f2926e5a0f5668a8330e59
Prapitesh <116258736+Prapitesh@users.noreply.github.com>
2026-01-14 12:44:33 +0530

    Handle empty string case in firstWord function

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
-  
+  if (s === "") return "";
+
+  let index = s.indexOf(" ");
+
+  if (index === -1) {
+    return s;
+  }
+
+  return s.substring(0, index);
 }
 
 // Do not change the code below
